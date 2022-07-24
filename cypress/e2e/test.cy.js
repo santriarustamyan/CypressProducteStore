b31eb1749653958ebdbbca79124b3d750506185b
santriaru <santriarustamyan93@gmail.com>
2022-07-24 17:56:30 +0400

    Initial commit

---
+++ cypress/e2e/test.cy.js
@@ -0,0 +1,35 @@
+import helper from "../support/helper"
+
+describe('network', () => {
+    before(() => {
+        cy.intercept("https://api.demoblaze.com/view").as("view")
+        cy.visit("https://demoblaze.com")
+    })
+
+    it('addToCart', () => {
+        cy.request({
+            method: "POST",
+            url: "https://api.demoblaze.com/view",
+            body: {"id": "1"}
+        }).its("body.title").should("equal", "Samsung galaxy s6");
+
+        cy.getCookie("user").then((c) => {
+            let cookie = "user=" + (c.value).toString();
+            cy.request({
+                method: "POST",
+                url: "https://api.demoblaze.com/addtocart",
+                body: {
+                    cookie: cookie,
+                    flag: false,
+                    id: helper.generateId(),
+                    prod_id: 1
+                }
+            })
+        })
+        cy.contains("Cart").click()
+        cy.wait("@view")
+        cy.get("#totalp").then(price => {
+            expect(price.text()).to.be.equal("360");
+        })
+    })
+})
